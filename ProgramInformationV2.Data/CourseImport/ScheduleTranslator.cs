a92921ed233df772bb8251c63ebf01208a8fbf94
Bryan Jonker <jonker@illinois.edu>
2025-11-25 09:21:53 -0600

    Parse course description text, add course hours and other fields

--- ProgramInformationV2.Data/CourseImport/ScheduleTranslator.cs
+++ ProgramInformationV2.Data/CourseImport/ScheduleTranslator.cs
@@ -11,15 +11,21 @@ namespace ProgramInformationV2.Data.CourseImport {
                 CourseNumber = scheduleCourse.CourseNumber,
                 Rubric = scheduleCourse.Rubric,
                 CourseTitle = scheduleCourse.Title,
-                Description = scheduleCourse.Description,
                 ExternalUrl = $"https://courses.illinois.edu/schedule/terms/{scheduleCourse.Rubric}/{scheduleCourse.CourseNumber}",
-                IsActive = true
+                IsActive = true,
+                CreditHours = scheduleCourse.CreditHours,
+                Description = string.Empty,
+                Information = string.Empty,
+                ScheduleInformation = string.Empty,
+                Prerequisite = string.Empty
             };
+            course = course.AddDescription(scheduleCourse.Description);
+
             if (includeSections) {
                 course.Sections = [.. scheduleCourse.Sections.Select(s => new Section {
                     Term = _termTranslation.TryGetValue(s.Term, out var value) ? value : Terms.None,
                     SectionCode = s.SectionNumber,
-                    CreditHours = s.CreditHours,
+                    CreditHours = string.IsNullOrWhiteSpace(s.CreditHours) ? course.CreditHours.Replace(" hours.", "") : scheduleCourse.CreditHours.Replace(" hours.", ""),
                     Description = s.SectionText + " " + s.SectionNotes,
                     BeginDate = DateTime.TryParse(s.Start?.Replace("Z", ""), out _) ? DateTime.Parse(s.Start?.Replace("Z", "") ?? "") : default,
                     EndDate = DateTime.TryParse(s.End?.Replace("Z", ""), out _) ? DateTime.Parse(s.End?.Replace("Z", "") ?? "") : default,
@@ -31,8 +37,21 @@ namespace ProgramInformationV2.Data.CourseImport {
                     DaysOfWeekList = Convert(s.DaysOfTheWeek),
                     FormatType = s.Type == "Online" ? FormatType.Online : FormatType.On__Campus,
                     FacultyNameList = s.Instructors == null ? [] : [.. s.Instructors.Select(i => new SectionFaculty { Name = i })],
-                    IsActive = true
+                    IsActive = true,
+                    Type = s.Type
                 })];
+            }
+            return course;
+        }
+
+        private static string AddColon(string a, string b) => string.IsNullOrWhiteSpace(a) ? b : $"{a}: {b}";
+
+        private static Course AddDescription(this Course course, string description) {
+            if (!string.IsNullOrWhiteSpace(description)) {
+                var array = description.Split(':');
+                for (var i = 0; i < array.Length; i++) {
+                    course = course.ParseDescription(array, i);
+                }
             }
             return course;
         }
@@ -58,5 +77,31 @@ namespace ProgramInformationV2.Data.CourseImport {
             }
             return returnValue;
         }
+
+        private static Course ParseDescription(this Course course, string[] s, int i) {
+            var text = s[i].RemoveLastText("Course Information").RemoveLastText("Class Schedule Information").RemoveLastText("Prerequisite").RemoveLastText("Information").Trim();
+            var isProcessed = false;
+            if (!string.IsNullOrWhiteSpace(text)) {
+                while (!isProcessed) {
+                    if (i == 0) {
+                        course.Description = AddColon(course.Description, text);
+                        isProcessed = true;
+                    } else if (s[i - 1].EndsWith("Class Schedule Information")) {
+                        course.ScheduleInformation = AddColon(course.ScheduleInformation, text);
+                        isProcessed = true;
+                    } else if (s[i - 1].EndsWith("Information") || s[i - 1].EndsWith("Course Information")) {
+                        course.Information = AddColon(course.Information, text);
+                        isProcessed = true;
+                    } else if (s[i - 1].EndsWith("Prerequisite")) {
+                        course.Prerequisite = AddColon(course.Prerequisite, text);
+                        isProcessed = true;
+                    }
+                    i -= 1;
+                }
+            }
+            return course;
+        }
+
+        private static string RemoveLastText(this string s, string removedText) => s.EndsWith(removedText) ? s.Remove(s.LastIndexOf(removedText, StringComparison.Ordinal), removedText.Length) : s;
     }
 }
