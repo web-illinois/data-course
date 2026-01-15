48c6af3b1e93ae38dc9a31a887027f7b0ca89c62
Bryan Jonker <jonker@illinois.edu>
2026-01-15 14:12:12 -0600

    Add skip/take to programs, fix search criteria

--- ProgramInformationV2.Data/DataHelpers/FilterHelper.cs
+++ ProgramInformationV2.Data/DataHelpers/FilterHelper.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
 using ProgramInformationV2.Data.DataContext;
 using ProgramInformationV2.Data.DataModels;
 using ProgramInformationV2.Search.Helpers;
 using ProgramInformationV2.Search.JsonThinModels;
-using System.Text.Json;
 
 namespace ProgramInformationV2.Data.DataHelpers {
 
@@ -16,20 +16,20 @@ namespace ProgramInformationV2.Data.DataHelpers {
 
         public async Task<List<TagList>> GetFilterListForExport(string source) => [.. (await GetAllFilters(source)).Select(t => new TagList {
                 Title = t.Key.ToString(),
-                List = [.. t.Select(l => l.Title)],
+                List = [.. t.OrderBy(l => l.Order).Select(l => l.Title)],
             })];
 
         public async Task<string> GetFilterJsonForExport(string source) => JsonSerializer.Serialize(await GetFilterListForExport(source));
 
         public async Task<string> ImportFiltersFromJson(string source, string json) {
-            List<TagList>? importedTags = JsonSerializer.Deserialize<List<TagList>>(json);
-            Source? sourceEntity = await _programRepository.ReadAsync(c => c.Sources.FirstOrDefault(s => s.Code == source));
+            var importedTags = JsonSerializer.Deserialize<List<TagList>>(json);
+            var sourceEntity = await _programRepository.ReadAsync(c => c.Sources.FirstOrDefault(s => s.Code == source));
             if (sourceEntity == null || importedTags == null) {
                 return "No tags loaded";
             }
-            foreach (TagList tagList in importedTags) {
+            foreach (var tagList in importedTags) {
                 var count = 1;
-                TagType tagType = Enum.TryParse<TagType>(tagList.Title, out TagType parsedTagType) ? parsedTagType : TagType.None;
+                var tagType = Enum.TryParse<TagType>(tagList.Title, out var parsedTagType) ? parsedTagType : TagType.None;
                 if (tagType != TagType.None) {
                     foreach (var tag in tagList.List) {
                         var newTag = new TagSource {
@@ -46,9 +46,9 @@ namespace ProgramInformationV2.Data.DataHelpers {
         }
 
         public async Task<(List<TagSource> TagSources, int SourceId)> GetFilters(string source, TagType tagType) {
-            List<TagSource> returnValue = await _programRepository.ReadAsync(c => c.TagSources.Include(c => c.Source).Where(ts => ts.Source != null && ts.Source.Code == source && ts.TagType == tagType).OrderBy(ts => ts.Order).ToList());
+            var returnValue = await _programRepository.ReadAsync(c => c.TagSources.Include(c => c.Source).Where(ts => ts.Source != null && ts.Source.Code == source && ts.TagType == tagType).OrderBy(ts => ts.Order).ToList());
             var sourceId = 0;
-            foreach (TagSource? item in returnValue) {
+            foreach (var item in returnValue) {
                 item.OldTitle = item.Title;
                 sourceId = item.SourceId;
             }
@@ -60,7 +60,7 @@ namespace ProgramInformationV2.Data.DataHelpers {
 
         public async Task<bool> SaveFilters(IEnumerable<TagSource> tags, IEnumerable<TagSource> tagsForDeletion, string sourceName) {
             var i = 1;
-            foreach (TagSource tag in tags) {
+            foreach (var tag in tags) {
                 tag.Order = i++;
                 if (tag.Id == 0) {
                     _ = await _programRepository.CreateAsync(tag);
@@ -72,7 +72,7 @@ namespace ProgramInformationV2.Data.DataHelpers {
                 }
             }
             if (_bulkEditor != null) {
-                foreach (TagSource tag in tagsForDeletion) {
+                foreach (var tag in tagsForDeletion) {
                     _ = await _programRepository.DeleteAsync(tag);
                     _ = await _bulkEditor.DeleteTags(sourceName, tag.TagTypeSourceName, tag.OldTitle);
                 }
