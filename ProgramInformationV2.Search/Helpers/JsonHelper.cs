d6343ab70908b09d87839faff9b5e4af5a843524
Bryan Jonker <jonker@illinois.edu>
2026-03-05 16:58:39 -0600

    Move to transfer, fix json load

--- ProgramInformationV2.Search/Helpers/JsonHelper.cs
+++ ProgramInformationV2.Search/Helpers/JsonHelper.cs
@@ -9,78 +9,83 @@ namespace ProgramInformationV2.Search.Helpers {
         private readonly OpenSearchClient _openSearchClient = openSearchClient ?? default!;
         private readonly OpenSearchLowLevelClient _openSearchLowLevelClient = openSearchLowLevelClient ?? default!;
 
-        public async Task<string> GetJson(string sourceCode, UrlTypes urltype, int page) {
-            var body = await _openSearchLowLevelClient.SearchAsync<StringResponse>(urltype.ConvertToUrlString(), GenerateGetJson(sourceCode, page));
+        public async Task<string> GetJson(string sourceCode, UrlTypes urlType, int page) {
+            var body = await _openSearchLowLevelClient.SearchAsync<StringResponse>(urlType.ConvertToUrlString(), GenerateGetJson(sourceCode, page));
             dynamic? json = JsonConvert.DeserializeObject(body.Body ?? "");
             return json == null || json?.hits == null || json?.hits.hits == null ? "error" : (string)JsonConvert.SerializeObject(json?.hits.hits);
         }
 
-        public async Task<string> GetJsonFull(string sourceCode, UrlTypes urltype) {
-            var body = await _openSearchLowLevelClient.SearchAsync<StringResponse>(urltype.ConvertToUrlString(), GenerateGetJson(sourceCode));
+        public async Task<string> GetJsonFull(string sourceCode, UrlTypes urlType) {
+            var body = await _openSearchLowLevelClient.SearchAsync<StringResponse>(urlType.ConvertToUrlString(), GenerateGetJson(sourceCode));
             dynamic? json = JsonConvert.DeserializeObject(body.Body ?? "");
             return json == null || json?.hits == null || json?.hits.hits == null ? "error" : (string)JsonConvert.SerializeObject(json?.hits.hits);
         }
 
-        public async Task<string> LoadJson(string sourceCode, UrlTypes urltype, string jsonString) {
+        public async Task<string> LoadJson(string sourceCode, UrlTypes urlType, string jsonString) {
             var json = JsonConvert.DeserializeObject<IEnumerable<dynamic>>(jsonString ?? "");
             var success = 0;
-            var failureIds = new List<string>();
             var useRawJsonItems = false;
+            var failureIds = new List<string>();
             if (json == null) {
                 return "error";
             }
             foreach (var jsonItem in json) {
+                var body = jsonItem.ToString();
                 if (jsonItem._source == null) {
                     useRawJsonItems = true;
-                    try {
-                        if (urltype == UrlTypes.Programs) {
-                            Program stronglyTypedProgram = JsonConvert.DeserializeObject<Program>(jsonItem.ToString());
+                } else {
+                    body = JsonConvert.SerializeObject(jsonItem._source);
+                }
+                try {
+                    switch (urlType) {
+                        case UrlTypes.Programs: {
+                            Program stronglyTypedProgram = JsonConvert.DeserializeObject<Program>(body);
+                            if (stronglyTypedProgram == null) {
+                                return "error";
+                            }
+                            foreach (var credential in stronglyTypedProgram.Credentials) {
+                                credential.Prepare();
+                            }
                             stronglyTypedProgram.Prepare();
-                            var response = await _openSearchClient.IndexAsync(stronglyTypedProgram, i => i.Index(urltype.ConvertToUrlString()));
+                            var response = await _openSearchClient.IndexAsync(stronglyTypedProgram, i => i.Index(urlType.ConvertToUrlString()));
                             if (response.IsValid) {
                                 success++;
                             } else {
                                 failureIds.Add("unknown program ID");
                             }
-                        } else if (urltype == UrlTypes.Courses) {
-                            Course stronglyTypedCourse = JsonConvert.DeserializeObject<Course>(jsonItem.ToString());
+                            break;
+                        }
+                        case UrlTypes.Courses: {
+                            Course stronglyTypedCourse = JsonConvert.DeserializeObject<Course>(body);
+                            foreach (var credential in stronglyTypedCourse.Sections) {
+                                credential.Prepare();
+                            }
                             stronglyTypedCourse.Prepare();
-                            var response = await _openSearchClient.IndexAsync(stronglyTypedCourse, i => i.Index(urltype.ConvertToUrlString()));
+                            var response = await _openSearchClient.IndexAsync(stronglyTypedCourse, i => i.Index(urlType.ConvertToUrlString()));
                             if (response.IsValid) {
                                 success++;
                             } else {
                                 failureIds.Add("unknown course ID");
                             }
-                        } else if (urltype == UrlTypes.RequirementSets) {
-                            RequirementSet stronglyTypedRequirementSet = JsonConvert.DeserializeObject<RequirementSet>(jsonItem.ToString());
+                            break;
+                        }
+                        case UrlTypes.RequirementSets: {
+                            RequirementSet stronglyTypedRequirementSet = JsonConvert.DeserializeObject<RequirementSet>(body);
                             stronglyTypedRequirementSet.Prepare();
-                            var response = await _openSearchClient.IndexAsync(stronglyTypedRequirementSet, i => i.Index(urltype.ConvertToUrlString()));
+                            var response = await _openSearchClient.IndexAsync(stronglyTypedRequirementSet, i => i.Index(urlType.ConvertToUrlString()));
                             if (response.IsValid) {
                                 success++;
                             } else {
                                 failureIds.Add("unknown requirement set ID");
                             }
-                        } else {
+                            break;
+                        }
+                        default:
                             failureIds.Add("unknown item from old system");
-                        }
-                    } catch (Exception e) {
-                        failureIds.Add($"Error processing item: {e.Message}");
+                            break;
                     }
-                } else {
-                    var body = JsonConvert.SerializeObject(jsonItem._source);
-                    string id = jsonItem._id.ToString();
-                    string source = jsonItem._source.source.ToString();
-                    string altId = jsonItem._source.id.ToString();
-                    if (id.StartsWith(sourceCode + "-") && altId.StartsWith(sourceCode + "-") && source == sourceCode) {
-                        var response = await _openSearchLowLevelClient.IndexAsync<StringResponse>(urltype.ConvertToUrlString(), id, body);
-                        if (response.Success) {
-                            success++;
-                        } else {
-                            failureIds.Add(id);
-                        }
-                    } else {
-                        failureIds.Add(id);
-                    }
+                } catch (Exception e) {
+                    failureIds.Add($"Error processing item: {e.Message}");
                 }
             }
             return failureIds.Count > 0
