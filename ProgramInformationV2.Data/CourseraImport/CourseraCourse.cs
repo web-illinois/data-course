7efe1fe0939c95f6302f04dd6ca9d27ab81f6488
Bryan Jonker <jonker@illinois.edu>
2026-06-09 17:04:03 -0500

    Add coursera import

---
+++ ProgramInformationV2.Data/CourseraImport/CourseraCourse.cs
@@ -0,0 +1,18 @@
+﻿namespace ProgramInformationV2.Data.CourseraImport {
+    public class CourseraCourse {
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string Url { get; set; } = "";
+        public string ImageUrl { get; set; } = "";
+        public string ImageAltText { get; set; } = "";
+
+        public bool IsCourseFree { get; set; }
+
+        public bool IsCreditEligible { get; set; }
+
+        public string Id { get; set; } = "";
+        public List<string> Skills { get; set; } = [];
+        public List<string> Instructors { get; set; } = [];
+
+    }
+}
