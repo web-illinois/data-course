bb2e16ff47353980bc47bf916a5e68e9698d33e7
Bryan Jonker <jonker@illinois.edu>
2026-08-07 08:37:33 -0500

    Minor changes to import

--- ProgramInformationV2.Function/LoadCourseFromSchedule.cs
+++ ProgramInformationV2.Function/LoadCourseFromSchedule.cs
@@ -1,10 +1,11 @@
-using System.Net;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Extensions.Logging;
 using ProgramInformationV2.Data.CourseImport;
 using ProgramInformationV2.Data.DataHelpers;
+using System.Net;
 
 namespace ProgramInformationV2.Function;
 
@@ -20,7 +21,10 @@ public class LoadCourseFromSchedule {
     }
 
     [Function("LoadCourseFromSchedule")]
+    [OpenApiOperation(operationId: "LoadCourseFromSchedule", tags: "Load Course From Schedule", Description = "Load Course that was scheduled using the UI. This will only do one at a time because of the load it causes on the courses.illinois.edu/cisapp process. Recommend only running this once every 10 seconds.")]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "Response describing what was done")]
     public async Task<HttpResponseData> ScheduledLoad([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestData req) {
+        _logger.LogInformation("Called LoadCourseFromSchedule.");
         var item = await _courseImportHelper.GetLatestPending();
         var response = req.CreateResponse(HttpStatusCode.OK);
         if (item == null) {
